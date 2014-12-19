00bc11a51f2b92afef330e594224e30d4ff12171
Dave Gynn <davegynn@gmail.com>
2014-12-19 09:13:57 -0800

    add helper method examples for new rails helper inspector

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def application_helper_example
+    'This is from the ApplicationHelper application_helper_example'
+  end
+
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,7 @@
 module WelcomeHelper
+
+  def welcome_helper_example
+    'This is from the WelcomeHelper welcome_helper_example'
+  end
+
 end
