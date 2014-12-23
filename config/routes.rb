df1acf5eadf5fdb1efb38160172b4e452828373d
Dave Gynn <davegynn@gmail.com>
2014-12-22 17:56:57 -0800

    add example routes (without controllers) to exercise new routes inspector

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: "welcome#index"
+
   devise_for :users
   devise_scope :user do
     get "/logout", :to => "devise/sessions#destroy"
@@ -9,6 +11,38 @@ Rails.application.routes.draw do
 
   get '/secure_page' => 'welcome#secure_page'
 
-  root to: "welcome#index"
+  # Sample routes
+  # Plural resource
+  resources :photos
+
+  # Singular resource
+  resource :geocoder
+
+  # Namespace
+  namespace :admin do
+    resources :articles, :comments
+  end
+
+  # Nested
+  resources :magazines do
+    resources :ads
+  end
+
+  # Dynamic non-RESTful
+  get ':controller(/:action(/:id))', controller: /admin\/[^\/]+/
+  get ':controller/:action/:id/with_user/:user_id'
+
+  # With defaults
+  get 'photos/:id', to: 'photos#show', defaults: { format: 'jpg' }
+
+  # With constraints
+  get 'photos_with_constraints/:id/:user_id', to: 'photos#show', constraints: { id: /[A-Z]\d{5}/, user_id: /[a-z]\d{4}/ }
+
+  # Route globbing
+  get 'books/*section/:title', to: 'books#show'
+
+  # Redirect
+  get '/stories', to: redirect('/articles')
+  get '/stories/:name', to: redirect { |path_params, req| "/articles/#{path_params[:name].pluralize}" }
 
 end
