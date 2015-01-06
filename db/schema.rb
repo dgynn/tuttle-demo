be6b0ddfd0465bb5caa54daa1e0520d38a49a6e0
Dave Gynn <davegynn@gmail.com>
2015-01-06 10:39:20 -0800

    switch to postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,11 @@
 
 ActiveRecord::Schema.define(version: 20140712041740) do
 
-  create_table "users", force: true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "hstore"
+
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -28,7 +32,7 @@ ActiveRecord::Schema.define(version: 20140712041740) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
