a850fae35e02623b7712798296603826bc94ed69
Dave Gynn <davegynn@gmail.com>
2015-02-01 17:12:34 -0800

    add direct cache access from cache controller in addition to view fragment caching

--- app/controllers/cache_controller.rb
+++ app/controllers/cache_controller.rb
@@ -1,6 +1,9 @@
 class CacheController < ApplicationController
 
   def index
+    Rails.cache.write('tuttle-demo.key1', 'Monday')
+    Rails.cache.read('tuttle-demo.key1')
+    Rails.cache.read('tuttle-demo.key2')
   end
 
 end
